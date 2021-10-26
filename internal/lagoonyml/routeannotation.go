7f1652325ef7fef204f6f34bb396a58081d9f0fb
Scott Leggett <scott@sl.id.au>
2021-10-26 17:15:57 +0800

    feat: update route annotation linter
    * disallow these annotations:
      * nginx.ingress.kubernetes.io/auth-snippet
      * nginx.ingress.kubernetes.io/configuration-snippet
      * nginx.ingress.kubernetes.io/modsecurity-snippet
    * allow server-snippet to contain multiple allowed directives in a
      single annotation

--- internal/lagoonyml/routeannotation.go
+++ internal/lagoonyml/routeannotation.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 )
 
-// ServerSnippet is the annotation for server snippets with ingress-nginx.
-const ServerSnippet = "nginx.ingress.kubernetes.io/server-snippet"
+const (
+	authSnippet          = "nginx.ingress.kubernetes.io/auth-snippet"
+	configurationSnippet = "nginx.ingress.kubernetes.io/configuration-snippet"
+	modsecuritySnippet   = "nginx.ingress.kubernetes.io/modsecurity-snippet"
+	serverSnippet        = "nginx.ingress.kubernetes.io/server-snippet"
+)
 
 // validSnippets is the allow-list of snippets that Lagoon will accept.
 var validServerSnippets = regexp.MustCompile(
@@ -17,7 +21,8 @@ var validServerSnippets = regexp.MustCompile(
 		` )+$`)
 
 // validate returns true if the annotations are valid, and false otherwise.
-func validate(annotations map[string]string, r *regexp.Regexp, annotation string) (string, bool) {
+func validate(annotations map[string]string, r *regexp.Regexp,
+	annotation string) (string, bool) {
 	if ss, ok := annotations[annotation]; ok {
 		for _, line := range strings.Split(ss, "\n") {
 			line = strings.TrimSpace(line)
@@ -37,10 +42,33 @@ func RouteAnnotation() Linter {
 				for rName, lagoonRoutes := range routeMap {
 					for _, lagoonRoute := range lagoonRoutes {
 						for iName, ingress := range lagoonRoute.Ingresses {
-							if annotation, ok := validate(ingress.Annotations, validServerSnippets, ServerSnippet); !ok {
+							// auth-snippet
+							if _, ok := ingress.Annotations[authSnippet]; ok {
 								return fmt.Errorf(
 									"invalid %s annotation on environment %s, route %s, ingress %s: %s",
-									ServerSnippet, eName, rName, iName, annotation)
+									authSnippet, eName, rName, iName,
+									"this annotation is restricted")
+							}
+							// configuration-snippet
+							if _, ok := ingress.Annotations[configurationSnippet]; ok {
+								return fmt.Errorf(
+									"invalid %s annotation on environment %s, route %s, ingress %s: %s",
+									configurationSnippet, eName, rName, iName,
+									"this annotation is restricted")
+							}
+							// modsecurity-snippet
+							if _, ok := ingress.Annotations[modsecuritySnippet]; ok {
+								return fmt.Errorf(
+									"invalid %s annotation on environment %s, route %s, ingress %s: %s",
+									modsecuritySnippet, eName, rName, iName,
+									"this annotation is restricted")
+							}
+							// server-snippet
+							if annotation, ok := validate(ingress.Annotations,
+								validServerSnippets, serverSnippet); !ok {
+								return fmt.Errorf(
+									"invalid %s annotation on environment %s, route %s, ingress %s: %s",
+									serverSnippet, eName, rName, iName, annotation)
 							}
 						}
 					}
