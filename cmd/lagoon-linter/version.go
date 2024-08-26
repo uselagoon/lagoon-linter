ad65e737fe8d5b90cc0c38fb93ee3bdc21791e37
Scott Leggett <scott@sl.id.au>
2024-08-26 12:19:34 +0800

    chore: update goreleaser config and version command

--- cmd/lagoon-linter/version.go
+++ cmd/lagoon-linter/version.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 // VersionCmd represents the version command.
 type VersionCmd struct{}
@@ -8,6 +11,6 @@ type VersionCmd struct{}
 // Run the version command to print version information.
 func (cmd *VersionCmd) Run() error {
 	fmt.Printf("lagoon-linter %v (%v) compiled with %v on %v\n", version,
-		shortCommit, goVersion, date)
+		shortCommit, runtime.Version(), date)
 	return nil
 }
